148296828c742438c127bc7974e76445a4e2b29d
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2015-05-20 21:57:46 -0300

    Remove autosize export and add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+##Changelog
+
+#####v0.0.1 –  2015-05-11
+* First draft version published with autosize v3.0.4
+
+#####v0.0.2 – 2015-05-20
+* Updated autosize to v3.0.5
+
+#####v0.0.3 – 2015-05-20
+* Added this CHANGELOG
+* Fix autosize object exporting
--- package.js
+++ package.js
@@ -1,11 +1,10 @@
 Package.describe({
   summary: 'Automatically adjust textarea height based on user input.',
-  version: '0.0.2',
+  version: '0.0.3',
   git: "https://github.com/bigu21/meteor-autosize.git",
   name: "bigu:autosize"
 });
 
 Package.on_use(function (api) {
   api.add_files(['vendor/autosize.min.js'], 'client');
-  api.export('autosize', 'client');
 });
